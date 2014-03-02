d41126fc381460147c65033eab14a967981d219d
Niklan <niklanrus@gmail.com>
2014-03-02 17:56:14 +0600

    + Added support to Search API + Facet API.

---
+++ themes/drupalife_store/templates/searchapi/search-api-ranges-block-slider-view-form.tpl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * @file custom Search API ranges Min/Max UI slider widget
+ */
+
+// We hide default text and make what we need.
+unset($form['text-range']);
+//print drupal_render($form['text-range']);
+$price_from = number_format($form['range-from']['#default_value'], 0) . ' руб.';
+$price_to = number_format($form['range-to']['#default_value'], 0) . ' руб.';
+$text_range = 'Показаны товары в промежутке от ' . $price_from . ' до ' . $price_to;
+?>
+<?php print $text_range; ?>
+<div class="yui3-g">
+  <div class="grid-full">
+    <div class="yui3-u range-box grid-1-2 left">
+      <?php print drupal_render($form['range-from']); ?>
+    </div>
+    <div class="yui3-u range-box grid-1-2 left">
+      <?php print drupal_render($form['range-to']); ?>
+    </div>
+  </div>
+
+  <div class="yui3-u range-slider-box">
+    <?php print drupal_render($form['range-slider']); ?>
+  </div>
+</div>
+<?php
+// Hide submit button, cuz we have autosubmit.
+$form['submit']['#attributes']['class'][] = 'element-invisible';
+print drupal_render($form['submit']);
+?>
+<?php
+  // Render required hidden fields.
+  print drupal_render_children($form);
+?>
